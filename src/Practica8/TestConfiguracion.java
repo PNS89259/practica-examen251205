ac22910029521a032381f714f32932e7bf173450
alumno35 <alumno35@10.0.2.15>
2025-12-05 04:28:00 +0100

    20251205 1er commit ficheros existentes

---
+++ src/Practica8/TestConfiguracion.java
@@ -0,0 +1,18 @@
+package Practica8;
+
+import java.util.Scanner;
+
+public class TestConfiguracion {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Escribe algo");
+	String algo = sc.nextLine();
+
+	int variableInutil = 10;
+
+	if (algo.equals("Hola")) {
+	    System.out.println("Adiós");
+	}
+    }
+}
